9e946fa5558345f147cba5c7d8428ad863bea12f
azizul <azizul45551@gmail.com>
2023-05-15 13:31:32 -0400

    Hello---------------------

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SAFWAN
+ *
+ */
+module abcd1 {
+}
---
+++ src/regi/RegistrationPage.java
@@ -0,0 +1,10 @@
+package regi;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
